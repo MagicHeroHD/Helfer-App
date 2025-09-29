a0110cde406dc21fd1b9f660d488ca8e478da3cd
MagicHeroHD <oskib2006@gmail.com>
2025-09-29 21:31:47 +0200

    Update sw.js
    einfacher

--- sw.js
+++ sw.js
@@ -8,15 +8,14 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  console.log('Service Worker installiert');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker aktiviert');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
